8be95b9db5ef1de698429ff158c1c4249badcba2
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-04-20 08:29:07 -0500

    Unit Tests not running because of naming convention. (#16)
    - Fixed bug in which unit tests were not being run and also when run would have failed (issue #17).
      - Fixed PSSA issues as well.
      - Fixed Markdown linting issues as well.

--- DSCResources/MSFT_xPowerShellExecutionPolicy/MSFT_xPowerShellExecutionPolicy.psm1
+++ DSCResources/MSFT_xPowerShellExecutionPolicy/MSFT_xPowerShellExecutionPolicy.psm1
@@ -1,4 +1,17 @@
-#--------------------------------------------------------------------------------- #The sample scripts are not supported under any Microsoft standard support #program or service. The sample scripts are provided AS IS without warranty  #of any kind. Microsoft further disclaims all implied warranties including,  #without limitation, any implied warranties of merchantability or of fitness for #a particular purpose. The entire risk arising out of the use or performance of  #the sample scripts and documentation remains with you. In no event shall #Microsoft, its authors, or anyone else involved in the creation, production, or #delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for loss of business profits, business interruption, #loss of business information, or other pecuniary loss) arising out of the use #of or inability to use the sample scripts or documentation, even if Microsoft #has been advised of the possibility of such damages #--------------------------------------------------------------------------------- 
+#--------------------------------------------------------------------------------- 
+#The sample scripts are not supported under any Microsoft standard support 
+#program or service. The sample scripts are provided AS IS without warranty  
+#of any kind. Microsoft further disclaims all implied warranties including,  
+#without limitation, any implied warranties of merchantability or of fitness for 
+#a particular purpose. The entire risk arising out of the use or performance of  
+#the sample scripts and documentation remains with you. In no event shall 
+#Microsoft, its authors, or anyone else involved in the creation, production, or 
+#delivery of the scripts be liable for any damages whatsoever (including, 
+#without limitation, damages for loss of business profits, business interruption, 
+#loss of business information, or other pecuniary loss) arising out of the use 
+#of or inability to use the sample scripts or documentation, even if Microsoft 
+#has been advised of the possibility of such damages 
+#--------------------------------------------------------------------------------- 
 
 function Get-TargetResource
 {
@@ -6,11 +19,11 @@ function Get-TargetResource
     [OutputType([System.Collections.Hashtable])]
     param
     (
-        [parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true)]
         [ValidateSet("Bypass","Restricted","AllSigned","RemoteSigned","Unrestricted")]
         [System.String]
         $ExecutionPolicy,
-        [parameter(Mandatory = $false)]
+        [Parameter()]
         [ValidateSet("CurrentUser","LocalMachine","MachinePolicy","Process","UserPolicy")]
         [System.String]
         $Scope = 'LocalMachine'
@@ -31,11 +44,11 @@ function Set-TargetResource
     [CmdletBinding(SupportsShouldProcess=$true)]
     param
     (
-        [parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true)]
         [ValidateSet("Bypass","Restricted","AllSigned","RemoteSigned","Unrestricted")]
         [System.String]
         $ExecutionPolicy,
-        [parameter(Mandatory = $false)]
+        [Parameter()]
         [ValidateSet("CurrentUser","LocalMachine","MachinePolicy","Process","UserPolicy")]
         [System.String]
         $Scope = 'LocalMachine'
@@ -69,11 +82,11 @@ function Test-TargetResource
     [OutputType([System.Boolean])]
     param
     (
-        [parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true)]
         [ValidateSet("Bypass","Restricted","AllSigned","RemoteSigned","Unrestricted")]
         [System.String]
         $ExecutionPolicy,
-        [parameter(Mandatory = $false)]
+        [Parameter()]
         [ValidateSet("CurrentUser","LocalMachine","MachinePolicy","Process","UserPolicy")]
         [System.String]
         $Scope = 'LocalMachine'
--- README.md
+++ README.md
@@ -1,17 +1,31 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/y2ohjd5q86oagghu/branch/master?svg=true)](https://ci.appveyor.com/project/PowerShell/xpowershellexecutionpolicy/branch/master)
-
 # xPowerShellExecutionPolicy
 
 The **xPowerShellExecutionPolicy** modules contains the **xPowerShellExecutionPolicy** DSC resource for configuring the PowerShell execution policy.
 
-
 This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
 For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+## Branches
+
+### master
+
+[![Build status](https://ci.appveyor.com/api/projects/status/y2ohjd5q86oagghu/branch/master?svg=true)](https://ci.appveyor.com/project/PowerShell/xPowerShellExecutionPolicy/branch/master)
+
+This is the branch containing the latest release -
+no contributions should be made directly to this branch.
+
+### dev
+
+[![Build status](https://ci.appveyor.com/api/projects/status/y2ohjd5q86oagghu/branch/dev?svg=true)](https://ci.appveyor.com/project/PowerShell/xPowerShellExecutionPolicy/branch/dev)
+
+This is the development branch
+to which contributions should be proposed by contributors as pull requests.
+This development branch will periodically be merged to the master branch,
+and be released to [PowerShell Gallery](https://www.powershellgallery.com/).
 
 ## Contributing
 
 Please check out common DSC Resources [contributing guidelines](https://github.com/PowerShell/DscResource.Kit/blob/master/CONTRIBUTING.md).
-
 
 ## Resources
 
@@ -20,10 +34,13 @@ Please check out common DSC Resources [contributing guidelines](https://github.c
 * **ExecutionPolicy**: Specifies the desired PowerShell execution policy.
 * **Scope**: Specifies the scope of the desired PowerShell execution policy. Defaults to 'LocalMachine'.
 
-
 ## Versions
 
 ### Unreleased
+
+* [Michael Fyffe (@TraGicCode)](https://github.com/TraGicCode): Fixed bug in which unit tests were not being run and also when run would have failed ([issue #17](https://github.com/PowerShell/xPowerShellExecutionPolicy/issues/17)).
+  * Fixed PSSA Issues as well.
+  * Fixed Markdown Linting issues as well.
 
 ### 2.0.0.0
 
@@ -36,12 +53,11 @@ Please check out common DSC Resources [contributing guidelines](https://github.c
 
 ### 1.0.0.0
 
-* Initial release with the following resources 
-    - xPowerShellExecutionPolicy
-
+* Initial release with the following resources
+  * xPowerShellExecutionPolicy
 
 ## Examples
 
-### Setting the PowerShell execution policy to Bypass 
+### Setting the PowerShell execution policy to Bypass
 
 In the Examples folder, SetPowerShellExecutionPolicy.ps1 is a sample configuration that demonstrates how to use xPowerShellExecutionPolicy to set the execution policy.
