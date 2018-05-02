318ab4b21a66232ed845c6127343de4ae6f66c5e
TraGicCode <6224270+TraGicCode@users.noreply.github.com>
2018-05-02 13:02:11 -0500

    Removing the todo comment from pr review.

--- Tests/Integration/MSFT_xPowerShellExecutionPolicy.config.ps1
+++ Tests/Integration/MSFT_xPowerShellExecutionPolicy.config.ps1
@@ -1,7 +1,6 @@
 
 # Integration Test Config Template Version: 1.0.0
 configuration MSFT_xPowerShellExecutionPolicy_config {
-    # TODO: Modify ModuleName (e.g. xNetworking)
     Import-DscResource -ModuleName 'xPowerShellExecutionPolicy'
     node localhost {
         xPowerShellExecutionPolicy Integration_Test
